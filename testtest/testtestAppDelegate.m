25b43618a7b368a3f2d2372b15d64a8d8f386f7e
shine <shine@meeit.com>
2011-07-19 19:05:55 +0800

    hkhgfkjvl

--- testtest/testtestAppDelegate.m
+++ testtest/testtestAppDelegate.m
@@ -16,7 +16,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    NSLog(@"test");
     [self.window makeKeyAndVisible];
     return YES;
 }
